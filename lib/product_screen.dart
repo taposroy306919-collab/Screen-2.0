06783ae86365bf1c68e4953190e29ef9f18b1d7b
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-04 15:26:03 +0600

    ListView Builder

--- lib/product_screen.dart
+++ lib/product_screen.dart
@@ -39,97 +39,102 @@ class _ProductScreenState extends State<ProductScreen> {
           )
         ],
       ),
-       body: Card(
-         child: SizedBox(
-           height: 150,
-           width: double.infinity,
-           child: Row(
-             children: [
-               Container(
-                 height: 135,
-                 width: 135,
-                 decoration: BoxDecoration(
-                   image: DecorationImage(
-                     fit: BoxFit.fill,
-                       image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy2galtw_kLVREPAY_PEOhPPVDiwEwPCecsvjnIAx4Rd7evK-dsHp1TPU&s=10"))
-                 ),
-               ),
-               SizedBox(width: 2,),
-               Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: [
-                   Text(
-                       "T-Shirt For Men",
-                     style: TextStyle(
-                         color: Colors.black,
-                         fontSize: 24,
-                         fontWeight: FontWeight.bold,
-                         fontStyle: FontStyle.italic
+       body: ListView.builder(
+         itemCount: 10,
+           itemBuilder: (context, index) {
+             return Card(
+               child: SizedBox(
+                 height: 150,
+                 width: double.infinity,
+                 child: Row(
+                   children: [
+                     Container(
+                       height: 135,
+                       width: 135,
+                       decoration: BoxDecoration(
+                           image: DecorationImage(
+                               fit: BoxFit.fill,
+                               image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy2galtw_kLVREPAY_PEOhPPVDiwEwPCecsvjnIAx4Rd7evK-dsHp1TPU&s=10"))
+                       ),
                      ),
-                   ),
-                   SizedBox(height: 5,),
-                   Row(
-                     children: [
-                       Text(
-                         "Brand : Easy",
-                         style: TextStyle(
-                             color: Colors.lightGreen,
-                             fontSize: 15,
-                             fontWeight: FontWeight.bold,
-                             fontStyle: FontStyle.italic
+                     SizedBox(width: 2,),
+                     Column(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       children: [
+                         Text(
+                           "T-Shirt For Men",
+                           style: TextStyle(
+                               color: Colors.black,
+                               fontSize: 24,
+                               fontWeight: FontWeight.bold,
+                               fontStyle: FontStyle.italic
+                           ),
                          ),
-                       ),
-                       SizedBox(width: 25,),
-                       Text(
-                         "Category : T-Shirt",
-                         style: TextStyle(
-                             color: Colors.lightGreen,
-                             fontSize: 15,
-                             fontWeight: FontWeight.bold,
-                             fontStyle: FontStyle.italic
+                         SizedBox(height: 5,),
+                         Row(
+                           children: [
+                             Text(
+                               "Brand : Easy",
+                               style: TextStyle(
+                                   color: Colors.lightGreen,
+                                   fontSize: 15,
+                                   fontWeight: FontWeight.bold,
+                                   fontStyle: FontStyle.italic
+                               ),
+                             ),
+                             SizedBox(width: 25,),
+                             Text(
+                               "Category : T-Shirt",
+                               style: TextStyle(
+                                   color: Colors.lightGreen,
+                                   fontSize: 15,
+                                   fontWeight: FontWeight.bold,
+                                   fontStyle: FontStyle.italic
+                               ),
+                             ),
+                           ],
                          ),
-                       ),
-                     ],
-                   ),
-                   SizedBox(height: 15,),
-                   Row(
-                     children: [
-                       Text(
-                         "Sale_rice : 400tk",
-                         style: TextStyle(
-                             color: Colors.black,
-                             fontSize: 12,
-                             fontWeight: FontWeight.bold,
-                             fontStyle: FontStyle.italic
+                         SizedBox(height: 15,),
+                         Row(
+                           children: [
+                             Text(
+                               "Sale_rice : 400tk",
+                               style: TextStyle(
+                                   color: Colors.black,
+                                   fontSize: 12,
+                                   fontWeight: FontWeight.bold,
+                                   fontStyle: FontStyle.italic
+                               ),
+                             ),
+                             SizedBox(width: 15,),
+                             Text(
+                               "Regular_price : 500tk",
+                               style: TextStyle(
+                                   color: Colors.black,
+                                   fontSize: 12,
+                                   fontWeight: FontWeight.bold,
+                                   fontStyle: FontStyle.italic
+                               ),
+                             ),
+                           ],
                          ),
+                       ],
+                     ),
+                     //SizedBox(width: 15,),
+                     Spacer(),
+                     CircleAvatar(
+                       radius: 30,
+                       child: Icon(
+                         Icons.add,
+                         color: Colors.green,
                        ),
-                       SizedBox(width: 15,),
-                       Text(
-                         "Regular_price : 500tk",
-                         style: TextStyle(
-                             color: Colors.black,
-                             fontSize: 12,
-                             fontWeight: FontWeight.bold,
-                             fontStyle: FontStyle.italic
-                         ),
-                       ),
-                     ],
-                   ),
-                 ],
-               ),
-               //SizedBox(width: 15,),
-               Spacer(),
-               CircleAvatar(
-                 radius: 30,
-                 child: Icon(
-                   Icons.add,
-                   color: Colors.green,
+                     )
+                   ],
                  ),
-               )
-             ],
-           ),
-         ),
-       ),
+               ),
+             );
+           },
+       )
     );
   }
 }
